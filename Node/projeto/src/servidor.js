fae9d1b0b5ec3d4c5eb22625c26329f67f0299af
Maciel Sousa <fmacielsousa@gmail.com>
2021-03-04 00:40:42 -0300

    API nodejs EXPRESS

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,10 +2,49 @@ const porta = 3003;
 
 const express = require('express');
 const app = express();
+const bodyParse = require('body-parser');
+const bancoDeDados = require('./bancoDeDados');
+
+app.use(bodyParse.urlencoded({ extended: true }));
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 }) //Converter para JSON
+    res.send(bancoDeDados.getProdutos()); //Converter para JSON
 });
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.put('/produtos/:id', (req, res, next) => {
+    console.log(req.params.id);
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+
+    res.send(produto); //JSON
+
+});
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.ExcluirProduto(req.params.id);
+    res.send(produto); //JSON
+
+});
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    });
+
+    res.send(produto); //JSON
+
+});
+
+
+
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`);
--- aondeParei.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-13. Lendo Arquivos
-=======
-18. Instalando o Postman
->>>>>>> d7dce41fc242d6cd28d7b4ddcdbe62a82461d217
